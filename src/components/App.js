0b05c2c8b5552c265ebeea61996d6edc1b1056b9
ImSteffi <steven.kristjan.rekand@gmail.com>
2024-02-02 21:26:23 +1100

    17. Redux pt 2. Integrated Exchange Smart Contract into app.js using Redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
 import { useEffect } from 'react'
 import config from '../config.json'
 import { useDispatch } from 'react-redux'
-import { loadProvider, loadNetwork, loadAccount, loadToken } from '../store/interactions'
+import { loadProvider, loadNetwork, loadAccount, loadTokens, loadExchange } from '../store/interactions'
 
 function App() {
 
   const dispatch = useDispatch()
 
   const loadBlockchainData = async () => {
-    await loadAccount(dispatch)
+    // Connect Ethers to blockchain
     const provider = loadProvider(dispatch)
+    // Fetch current network's chainId (e.g hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch)
-    await loadToken(provider, config[chainId].STEK.address, dispatch)
-    
+    // Fetch current account & balance from Metamask
+    await loadAccount(provider, dispatch)
+    // Load token smart contracts
+    const STEK = config[chainId].STEK
+    const mETH= config[chainId].mETH
+    await loadTokens(provider, [STEK.address, mETH.address], dispatch)
+    // Load exchange smart contract
+    const exchangeConfig = config[chainId].exchange
+    await loadExchange(provider, exchangeConfig.address, dispatch)
   }
 
   useEffect(() => {
